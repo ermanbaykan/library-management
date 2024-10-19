8ce78ab3716485a06f3b94fdee0a007e92fd81d8
ermanbaykan <ermanbaykan@gmail.com>
2024-10-19 14:23:49 +0100

    init project

---
+++ src/main/java/co/uk/e2b/identitye2e/assessment/librarymanagement/LibraryManagementApplication.java
@@ -0,0 +1,13 @@
+package co.uk.e2b.identitye2e.assessment.librarymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LibraryManagementApplication.class, args);
+  }
+
+}
---
+++ src/test/java/co/uk/e2b/identitye2e/assessment/librarymanagement/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uk.e2b.identitye2e.assessment.librarymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
